dffc4c4259c4bb2d48cb3660e6e2e12caec00fff
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2021-01-18 08:30:01 -0600

    blog commit

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,55 @@
+
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>{{ title }}</h1>
+    <Navbar />
+    <AllFriends :friends="friends" @delete="deleteFriend" />
+    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
+import Navbar from "./NavBar.vue";
+import AllFriends from "./AllFriends";
+import OnlineFriends from "./AllOnlineFriends";
+import Blogs from "./Blogs";
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  components: {
+    Navbar,
+    AllFriends,
+    OnlineFriends,
+    AllFriends,
+    Blogs,
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      title: "Bienvenido al 2021, Allan",
+      friends: [
+        { name: "Allan", online: true },
+        { name: "Toad", online: false },
+        { name: "Bryan", online: true },
+        { name: "Jason", online: false },
+      ],
+    };
+  },
+  methods: {
+    deleteFriend(payload) {
+      console.log(payload);
+      this.friends = this.friends.filter((friend) => {
+        return friend.name !== payload;
+      });
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1 {
+  color: blueviolet;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
---
+++ src/Blogs.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="blogs">
+    <h2>Posts</h2>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body | snippet }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  methods: {},
+
+  created() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      console.log(response);
+      this.posts = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
